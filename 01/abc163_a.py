58554f5657af4f0cb8715f1f1d4672d91f55cb64
fumiyanll23 <02index04@gmail.com>
2021-03-19 19:10:47 +0900

    Add 01/abc1??_a.py

---
+++ 01/abc163_a.py
@@ -0,0 +1,16 @@
+# ABC163A - Circle Pond
+
+import math
+
+def main():
+    # input
+    R = int(input())
+
+    # compute
+
+    # output
+    print(2 * math.pi * R)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 01/abc172_a.py
@@ -0,0 +1,14 @@
+# ABC172A - Calc
+
+def main():
+    # input
+    a = int(input())
+
+    # compute
+
+    # output
+    print(a + a**2 + a**3)
+
+
+if __name__ == '__main__':
+    main()
