4aee01344983dcff0a27c3d6ccb232a7b9413ffb
fumiyanll23 <02index04@gmail.com>
2021-03-20 17:58:43 +0900

    Add prob/abc1??_?.py

---
+++ 01/src/prob/abc163_a.py
@@ -0,0 +1,16 @@
+# ABC163A - Circle Pond
+
+# WRITE HERE
+
+def main():
+    # input
+    R = int(input())
+
+    # compute
+
+    # output
+    print(# WRITE HERE)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 01/src/prob/abc183_b.py
@@ -0,0 +1,14 @@
+# ABC183B - Billiards
+
+def main():
+    # input
+    SX, SY, GX, GY = map(int, input().split())
+
+    # compute
+
+    # output
+    print(# WRITE HERE)
+
+
+if __name__ == '__main__':
+    main()
