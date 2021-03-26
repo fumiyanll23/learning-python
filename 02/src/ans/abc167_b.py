0608c47139d3af44cf259e3c000228080756c72c
fumiyanll23 <02index04@gmail.com>
2021-03-26 22:59:08 +0900

    Add ans/ abc167_b.py

---
+++ 02/src/ans/abc167_b.py
@@ -0,0 +1,17 @@
+def main():
+    # input
+    A, B, C, K = map(int, input().split())
+
+    # compute
+
+    # output
+    if(K <= A):
+        print(K)
+    elif(K > A and K <= A+B):
+        print(A)
+    else:
+        print(A - (K - (A+B)))
+
+
+if __name__ == '__main__':
+    main()
