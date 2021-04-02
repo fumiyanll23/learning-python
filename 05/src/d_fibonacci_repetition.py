17230493dd36681c67fdfc324d278d0de8d08c57
Fumiyan <61838521+fumiyanll23@users.noreply.github.com>
2021-04-03 00:36:36 +0900

    Update d_fibonacci_repetition.py

--- 05/src/d_fibonacci_repetition.py
+++ 05/src/d_fibonacci_repetition.py
@@ -3,14 +3,9 @@
 def fibonacci(N: int) -> int:
     x, y = 1, 1
 
-    if N == 1:
-        return x
-    elif N == 2:
-        return y
-    else:
-        for _ in range(N-2):
-            x, y = y, x+y
-        return y
+    for _ in range(N-2):
+        x, y = y, x+y
+    return y
 
 def main():
     # input
